b566e944f01ef6347d309b2c170de69b7dc9dbf7
collins koech <collinskoech875@gmail.com>
2020-10-20 16:12:00 +0300

    add classifier class and init function

--- Classifier.py
+++ Classifier.py
@@ -13,3 +13,14 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 import constants
+
+class Classifier(Thread):
+    def __init__(self):
+        super().__init__()
+        self._vectorizer = deserialize(
+            constants.SERIALIZED_COUNT_VECTORIZER_FILENAME)
+        self._classifier = deserialize(constants.SERIALIZED_CLASSIFIER_FILENAME)
+        self._client = MongoClient()
+        self._db = self._client['test']
+        self.set_stop_words()
+        self.start()
